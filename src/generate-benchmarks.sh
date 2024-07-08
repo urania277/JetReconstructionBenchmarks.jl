10636da4e027b618958770ae485a7155842853a1
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-07-08 10:27:02 +0200

    Updated benchmarks with R/algorithm sweeps

--- src/generate-benchmarks.sh
+++ src/generate-benchmarks.sh
@@ -12,9 +12,11 @@ inputs=data/events-summary-ee-pp.csv
 # Iterate over backends
 for backend in Julia FastJet; do
     for strategy in N2Plain N2Tiled; do
-        for radius in 0.4 1.0; do
-            echo Generating results for $backend at radius $radius using $strategy
-            julia --project src/benchmark.jl --backend $backend -R $radius -A AntiKt -S $strategy -m 16 --results $output $inputs
+        for radius in 0.2 0.4 1.0 1.5 2.0 3.0; do
+            for algorithm in AntiKt CA Kt; do
+                echo "Benchmark $backend $radius $algorithm $strategy:"
+                julia --project src/benchmark.jl --backend $backend -R $radius -A $algorithm -S $strategy -m 16 --results $output $inputs
+            done
         done
     done
 done
