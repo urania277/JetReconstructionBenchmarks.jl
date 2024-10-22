663602ea651f249587e9bf1eb691ca7b76f00600
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-10-22 18:54:58 +0200

    Template commands from pp and e+e-

--- src/thread-scan.sh
+++ src/thread-scan.sh
@@ -10,6 +10,7 @@ max_threads=${1:-"10"}
 echo "threads,time_per_event,event_rate"
 threads=1
 while [ $threads -le $max_threads ]; do
-    julia --threads=$threads --project src/thread-run.jl -A AntiKt -R 0.4 --repeats 4 --nsamples 32 data/events-pp-13TeV-20GeV.hepmc3
+    # julia --threads=$threads --project src/thread-run.jl -A Durham --repeats 4 --nsamples 100 data/events-ee-H.hepmc3.gz
+    julia --threads=$threads --project src/thread-run.jl -A AntiKt -S N2Tiled -R 0.4 --repeats 4 --nsamples 32 data/events-pp-13TeV-20GeV.hepmc3.gz
     threads=$(($threads+1))
 done
