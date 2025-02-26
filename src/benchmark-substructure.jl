967d207c37020c0edd4b7ff4964b0269ab0b8740
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2025-02-26 23:21:33 +0100

    Update substructure benchmark
    Output the more sensible average times per operation (just println() for
    now...)

--- src/benchmark-substructure.jl
+++ src/benchmark-substructure.jl
@@ -162,83 +162,17 @@ function main()
         CSV.write(args[:results], hepmc3_files_df)
     end
     
+    # Now calculate the global average for the 4 substructure functions
+    # @infiltrate
+    gavg_filter = sum(hepmc3_files_df[:, :ExJets] .* hepmc3_files_df[:, :filter_time]) / sum(hepmc3_files_df[:, :ExJets])
+    gavg_trim = sum(hepmc3_files_df[:, :ExJets] .* hepmc3_files_df[:, :trimming_time]) / sum(hepmc3_files_df[:, :ExJets])
+    gavg_softd = sum(hepmc3_files_df[:, :ExJets] .* hepmc3_files_df[:, :softdrop_time]) / sum(hepmc3_files_df[:, :ExJets])
+    gavg_massd = sum(hepmc3_files_df[:, :ExJets] .* hepmc3_files_df[:, :massdrop_time]) / sum(hepmc3_files_df[:, :ExJets])
+
+    println("Filter: $gavg_filter")
+    println("Trim: $gavg_trim")
+    println("Massdrop: $gavg_massd")
+    println("Softdrop: $gavg_softd")
 end
-
-
-# filter_timing = Float64[]
-# trimming_timing = Float64[]
-# massdrop_timing = Float64[]
-# softdrop_timing = Float64[]
-
-# n_samples = Int[]
-
-# for event_file in hepmc3_files_df[:, :File_path]
-#     allEvents = read_final_state_particles(event_file)
-#     event_num = length(allEvents)
-#     avg_filter_time = 0.0
-#     avg_trimming_time = 0.0
-#     avg_massdrop_time = 0.0
-#     avg_softdrop_time = 0.0
-#     total_inclusive_jets = 0
-#     warm_up = 0.0
-
-#     push!(n_samples, args[:nsamples])
-
-#     if args[:nsamples] > 1
-#         @info "Doing initial warm-up run"
-#         for event in allEvents
-#             cluster = makeJets(event)
-#             allJets = inclusive_jets(cluster; ptmin = 2.0, T = PseudoJet)
-#             total_inclusive_jets += length(allJets)
-
-#             warm_up += @elapsed [jet_filtering(jet, cluster, args[:filter]) for jet in allJets]
-#             warm_up += @elapsed [jet_trimming(jet, cluster, args[:trim]) for jet in allJets]
-#             warm_up += @elapsed [mass_drop(jet, cluster, args[:massdrop]) for jet in allJets]
-#             warm_up += @elapsed [soft_drop(jet, cluster, args[:softdrop]) for jet in allJets]
-#         end
-#     end
-#     println("File $event_file - got $total_inclusive_jets inclusive jets")
-#     println("Warm up was $warm_up")
-
-#     for j in 1:args[:nsamples]
-#         global counts = 0
-#         for event in allEvents
-#             counts += length(event)
-#             cluster = makeJets(event)
-#             allJets = inclusive_jets(cluster; ptmin = 2.0, T = PseudoJet)
-
-#             avg_filter_time += @elapsed [jet_filtering(jet, cluster, args[:filter]) for jet in allJets]
-#             avg_trimming_time += @elapsed [jet_trimming(jet, cluster, args[:trim]) for jet in allJets]
-#             avg_massdrop_time += @elapsed [mass_drop(jet, cluster, args[:massdrop]) for jet in allJets]
-#             avg_softdrop_time += @elapsed [soft_drop(jet, cluster, args[:softdrop]) for jet in allJets]
-#         end
-
-#         counts /= event_num 
-#     end
-
-#     avg_filter_time /= args[:nsamples] * 10 ^ -4 
-#     avg_trimming_time /= args[:nsamples] * 10 ^ -4
-#     avg_massdrop_time /= args[:nsamples] * 10 ^ -4
-#     avg_softdrop_time /= args[:nsamples] * 10 ^ -4
-
-#     push!(filter_timing, avg_filter_time)
-#     push!(trimming_timing, avg_trimming_time)
-#     push!(massdrop_timing, avg_massdrop_time)
-#     push!(softdrop_timing, avg_softdrop_time)
-
-# end
-
-# hepmc3_files_df[:, :n_samples] = n_samples
-# hepmc3_files_df[:, :filter_time] = filter_timing
-# hepmc3_files_df[:, :trimming_time] = trimming_timing
-# hepmc3_files_df[:, :massdrop_time] = massdrop_timing
-# hepmc3_files_df[:, :softdrop_time] = softdrop_timing
-
-# println(hepmc3_files_df)
-
-# if !isnothing(args[:results])
-# 	CSV.write(args[:results], hepmc3_files_df)
-# end
-# end
 
 main()
